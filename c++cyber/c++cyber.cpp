290a1b52285dde69986bee8f978eea9d12d66e40
Hardwik1337 <karta007pryadko@gmail.com>
2024-04-02 17:18:30 +0300

    Add project files.

---
+++ c++cyber/c++cyber.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ccyber
+{
+	TEST_CLASS(ccyber)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
